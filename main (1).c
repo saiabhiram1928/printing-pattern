d3515dd6b1db565ed5c052c7b786b30d5d0df87d
saiabhiram1928 <75559376+saiabhiram1928@users.noreply.github.com>
2020-12-08 16:36:20 +0530

    Add files via upload

---
+++ main (1).c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+int main()
+{ int a=6,i,n;
+for(n=1;n<=a;n++){
+    for(i=1;i<=n;i++){
+        printf("01");
+        
+    }printf(" ");
+}
+
+  
+   
+
+ 
+
+ 
+ 
+    return 0;
+}
+
